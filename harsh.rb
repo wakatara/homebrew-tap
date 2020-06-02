cb20a5fdb1a3cf1e217ac5d55080ec7e005b9d71
goreleaserbot <goreleaser@carlosbecker.com>
2020-06-03 01:38:56 +0800

    Brew formula update for harsh version v0.8.3

--- harsh.rb
+++ harsh.rb
@@ -2,16 +2,16 @@
 class Harsh < Formula
   desc "habit tracking for geeks. A minimalist CLI for examining your habits."
   homepage "https://github.com/wakatara/harsh"
-  version "0.8.2"
+  version "0.8.3"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/wakatara/harsh/releases/download/v0.8.2/harsh_Darwin_x86_64.tar.gz"
-    sha256 "2067e911128c839aa47f0bb3abffa9b973a3b24647818ddce91a37a8a9b86984"
+    url "https://github.com/wakatara/harsh/releases/download/v0.8.3/harsh_Darwin_x86_64.tar.gz"
+    sha256 "c9b7d6f75845c7db55555c609b8253b9a1d43a4f04f7a64b6dffd55b0fc09a33"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/wakatara/harsh/releases/download/v0.8.2/harsh_Linux_x86_64.tar.gz"
-      sha256 "cc52080cc4b888aa9502f4ecb58d7f88c12cab78e0a5b6946deb95054805002c"
+      url "https://github.com/wakatara/harsh/releases/download/v0.8.3/harsh_Linux_x86_64.tar.gz"
+      sha256 "a3350b0955b01942cb9ea6b586e8960cf2d6e6a047e653bc49a38f74b841c8cc"
     end
   end
 
